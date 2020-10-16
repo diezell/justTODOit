7713e9efd7cd77d25e35a62363edd07d3a60d4e3
Max <marshakov1996@gmail.com>
2020-10-16 18:14:21 +0700

    create project

---
+++ src/main/java/com/application/todoit/TodoitApplication.java
@@ -0,0 +1,12 @@
+package com.application.todoit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoitApplication.class, args);
+    }
+}
---
+++ src/test/java/com/application/todoit/TodoitApplicationTests.java
@@ -0,0 +1,12 @@
+package com.application.todoit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
